b91683c4747ed45bbc4e9470a16fc3b1890137a0
Romain Thomas <me@romainthomas.fr>
2020-12-05 16:37:18 +0100

    WIP

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -162,7 +162,7 @@ eval $(ssh-agent -s)
 set +x # IMPORTANT
 openssl aes-256-cbc -K $LIEF_AUTOMATIC_BUILDS_KEY -iv $LIEF_AUTOMATIC_BUILDS_IV -in "$LIEF_SRCDIR/.github/deploy-key.enc" -out .git/deploy-key -d
 set -x
-chmod 600 .git/deploy-key
+chmod 400 .git/deploy-key
 ssh-add .git/deploy-key
 fix_home_ssh_perms
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
